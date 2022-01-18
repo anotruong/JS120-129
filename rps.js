877e21e4fb0237f786d4a9ebd36876c1cb0446e3
Mel <74751155+anotruong@users.noreply.github.com>
2022-01-17 16:24:04 -0800

    Update rps.js
    Nearly complete, there is a bug somewhere that happens right after 'human.choice' is invoked. Throws off game.

--- rps.js
+++ rps.js
@@ -1,55 +1,43 @@
-/* Assignment: OO Rock Paper Scissors
+/* Assignment: OO Rock Paper Scissors Bonus Features
 
 Explicit: 
-  The user makes a choice.
-  The computer makes a choice.
-  The winner is displayed. 
-  The Classical Appoarch
-    -Write a textual description of the problem or exercise
-    -Extract the significant nounds and verbs from descrip.
-    -Organize and associate the verbs with the nouns. 
-      Nouns: Objects, types of objects
-      Verbs: behavior, operations
-    
-Implicit:
-
-The Classical Approach:
-  Textual description of the exercise or problem
-    -RPS is a two-player game where each player chooses one of three possible moves: rock, paper, or scissors. The winner is chosen by comparing their moves with the following rules:
-      Rock crushes scissors, i.e., rock wins against scissors.
-      Scissors cuts paper, i.e., scissors beats paper.
-      Paper wraps rock, i.e., paper beats rock.
-      If the players chose the same move, the game is a tie.
-  
-  Extract the significant nounds and verbs:
-    -Nouns: player, move rule
-    -Verbs: chose, compare
-      Note: 'rock', 'paper' and 'scissors' is a variation of a move, but each move has a different state so they are considered nouns. 
-  
-  Organize and associate the verbs with the nouns. 
-    -Player:
-      -choose
-    Move
-    Rule
-
-    Computer:
-    -compare
-
+  Keeping Score // done
+  Add Lizard and Spock // done
+    -Lizard
+      -Eats paper
+      -Rock smashes lizard
+      -Scissors decapitates Lizard
+      -poisons Spock 
+    -Spock 
+      -Smashes scissors
+      -vaporizes rock
+      -Lizard poisons Spock
+      -Paper disproves Spock
+  Keep Track of a history of moves //done
+    -Game ends when user gives up. 
+  Adjust Computer choice based on history
+    -Ex. If the human wins over 60% of their hands when the computer chooses 'rock, then reduce the likelihood that the computer chooses rock. 
+    -Analyysis the history analysis. 
+      Compile an object of losses against the human. 
+        record the occurances of the losses. 
+      Find the key with the highest value
+      If the computer's move is the same key of the highest value
+        then make a new choice. 
 */
 
 const readline = require('readline-sync');
 
 function createComputer() {
   let playerObject = createPlayer();
-  let computerObject= {
+  let computerObject = {
     choose() {
-      const choices = ['rock', 'paper', 'scissors'];
+      const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
       let randomIndex = Math.floor(Math.random() * choices.length);
       this.move = choices[randomIndex];
     },
   };
 
-  return Object.assign(playerObject, computerObject)
+  return Object.assign(playerObject, computerObject);
 }
 
 function createHuman() {
@@ -59,9 +47,9 @@ function createHuman() {
       let choice;
 
       while (true) {
-        console.log('Please choose rock, paper, or scissors:');
+        console.log('\nPlease choose rock, paper, scissors, lizard or spock:');
         choice = readline.question();
-        if (['rock', 'paper', 'scissors'].includes(choice)) break;
+        if ((['rock', 'paper', 'scissors', 'spock', 'lizard']).includes(choice)) break;
         console.log('Sorry, invalid choice.');
       }
 
@@ -81,52 +69,117 @@ function createPlayer() {
 const RPSGame = {
   human: createHuman(),
   computer: createComputer(),
+  humanScore: 0,
+  compScore: 0,
+  humanMoves: [],
+  compMoves: [],
+  recordOfCompLosses: {
+      rock: 0,
+      paper: 0,
+      scissors: 0,
+      lizard: 0,
+      spock: 0
+    },    
+
+  compCounterAttack() {
+    let losingChoice = Object.entries(this.recordOfCompLosses)
+                             .sort((a, b) => b[1] - a[1])[0][0];
+    return losingChoice;
+  },
 
   displayWelcomeMessage() {
-    console.log('Welcome to Rock, Paper, Scissors!');
+    console.log('Welcome to Rock, Paper, Scissors, Lizard, SPOCK!');
   },
 
   displayGoodbyeMessage() {
-    console.log('Thanks for playing Rock, Paper, Scissors. Goodbye!');
+    console.log('Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!');
+  },
+
+  displayScoreboard() {
+    let dashLength = (this.humanScore + this.compScore);
+    dashLength = dashLength.toString().length + 27;
+    
+    console.log(`   +${'-'.repeat(dashLength)}+`);
+    console.log(`   |  computer: ${this.compScore}  |  human: ${this.humanScore}  |`)
+    console.log(`   +${'-'.repeat(dashLength)}+`);
+    
+    this.humanMoves.forEach((move, idx) => {
+      let humanWhtSpc = 9 - (move.length);
+      let compWhtSpc = 11 - (this.compMoves[idx].length);
+      let roundWhtSpc = (idx + 1).toString().length=== 1 ? 1 : 0;
+
+      console.log(`R${idx+ 1}${' '.repeat(roundWhtSpc)}|    ${this.compMoves[idx]}${' '.repeat(compWhtSpc)}|   ${move}${' '.repeat(humanWhtSpc)}|`);
+    })
   },
 
   displayWinner() {
     let humanMove = this.human.move;
     let computerMove = this.computer.move;
 
-    console.log(`You chose: ${this.human.move}`);
+    while (computerMove === this.compCounterAttack()) {
+      computerMove = this.computer.move;
+    }
+
+    this.humanMoves.push(this.human.move);
+    this.compMoves.push(this.computer.move);
+
+    console.log(`\nYou chose: ${this.human.move}`);
     console.log(`The computer chose: ${this.computer.move}`);
 
     if ((humanMove === 'rock' && computerMove === 'scissors') ||
+        (humanMove === 'rock' && computerMove === 'lizard') ||
         (humanMove === 'paper' && computerMove === 'rock') ||
-        (humanMove === 'scissors' && computerMove === 'paper')) {
+        (humanMove === 'paper' && computerMove === 'spock') ||
+        (humanMove === 'scissors' && computerMove === 'paper') ||
+        (humanMove === 'scissors' && computerMove === 'lizard') ||
+        (humanMove === 'lizard' && computerMove === 'paper') ||
+        (humanMove === 'lizard' && computerMove === 'spock') ||
+        (humanMove === 'spock' && computerMove === 'rock') ||
+        (humanMove === 'spock' && computerMove === 'scissors')) {
           console.log('You win!');
-        } else if ((humanMove === 'rock' && computerMove === 'paper') ||
-                   (humanMove === 'paper' && computerMove === 'scissors') ||
-                   (humanMove === 'scissors' && computerMove === 'rock')){
-          console.log('Computer wins!')
+          this.humanScore += 1;
+        } else if ((computerMove === 'rock' && humanMove === 'scissors') ||
+                   (computerMove === 'rock' && humanMove === 'lizard') ||
+                   (computerMove === 'paper' && humanMove === 'rock') ||
+                   (computerMove === 'paper' && humanMove === 'spock') ||
+                   (computerMove === 'scissors' && humanMove === 'paper') ||
+                   (computerMove === 'scissors' && humanMove === 'lizard') |
+                   (computerMove === 'lizard' && humanMove === 'paper') ||
+                   (computerMove === 'lizard' && humanMove === 'spock') ||
+                   (computerMove === 'spock' && humanMove === 'rock') ||
+                   (computerMove === 'spock' && humanMove === 'scissors')){
+          console.log('Computer wins!');
+          this.recordOfCompLosses[computerMove] += 1;
+          this.compScore += 1;
         } else {
           console.log("It's a tie.");
         }
   },
 
   playAgain() {
-    console.log('Would you like to play again? (y/n');
+    console.log(`\nWould you like to play again? (y/n)`);
     let answer = readline.question();
     return answer.toLowerCase()[0] === 'y';
   },
 
   play() {
+    console.clear();
+
     this.displayWelcomeMessage();
     while(true) {
+      this.displayScoreboard();
       this.human.choose();
       this.computer.choose();
+
+      this.displayScoreboard();
       this.displayWinner();
       if (!this.playAgain()) break;
+
+      console.clear();
     }
 
     this.displayGoodbyeMessage();
   },
-};
+}
 
 RPSGame.play();
